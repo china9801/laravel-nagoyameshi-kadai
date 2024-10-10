16260f71f303694897350eaef64fdc9a8506f09b
china9801 <x17f007@gmail.com>
2024-10-10 22:57:48 +0900

    管理者画面のエラー

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Restaurant;
+use App\Models\Category;
 
 class HomeController extends Controller
 {
