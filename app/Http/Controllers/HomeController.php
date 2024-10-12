4671ac7a4c310319ac489db7f8739ce62f63d60c
china9801 <x17f007@gmail.com>
2024-10-12 21:53:25 +0900

    仮実装

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,8 @@ class HomeController extends Controller
     public function index() {
         // データを取得
         $highly_rated_restaurants = Restaurant::take(6)->get(); // レビュー機能実装後に並び替え
-        $categories = Category::all(); // すべてのカテゴリ
+        // $categories = Category::all(); // すべてのカテゴリ
+        $categories = ['和食','デザート','洋食','中華'];
         $new_restaurants = Restaurant::orderBy('created_at', 'desc')->take(6)->get(); // 新しい順に取得
 
         // ビューにデータを渡す
