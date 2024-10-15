feb8c2a93870df5c57be520bc06f5bcdb5bf33e4
china9801 <x17f007@gmail.com>
2024-10-15 10:00:53 +0900

    カテゴリ機能の編集

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,16 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
 class Category extends Model
 {
     use HasFactory;
-
-    public function restaurants() {
-        return $this->belongsToMany(Restaurant::class)->withTimestamps();
-    }
-
-    protected $fillable = [
-        'name',
-    ];
 }
